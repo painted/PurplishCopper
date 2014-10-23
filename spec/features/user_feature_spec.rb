a1306d73c570a51121ee036ae583a2712f2150f8
Painted <will.allen@paintedltd.co.uk>
2014-10-23 12:32:39 +0100

    user can now sign up

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -6,5 +6,14 @@ describe 'users' do
 			visit '/users/sign_in'
 			expect(page).to have_content 'Sign up'
 		end
+
+		it 'can create an account' do 
+			visit '/users/sign_up'
+			fill_in 'Email', with: 'test@test.com'
+			fill_in 'Password', with: '12345678'
+			fill_in 'Password confirmation', with: '12345678'
+			click_button 'Sign up'
+			expect(page).to have_content 'Welcome'
+		end
 	end
 end
