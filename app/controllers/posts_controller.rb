7f839436503152157d590668d0a917bd360ac8f6
Painted <will.allen@paintedltd.co.uk>
2014-10-23 14:41:35 +0100

    added post form and index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,14 @@
 class PostsController < ApplicationController
 	def index
+		@posts = Post.all
+	end
+
+	def new 
+		@post = Post.new
+	end
+
+	def create
+		Post.create(params[:post].permit(:title, :description))
+		redirect_to user_posts_path
 	end
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -10,10 +10,18 @@ describe 'posts' do
 			end
 
 			it 'has no posts initially.' do
-				user = User.create(email: 'test@test.com', password: '12345678', password_confirmation:'12345678')
 				visit "users/1/posts"
 				expect(page).to have_content 'no posts yet.'
 			end
+
+			it 'can create a post' do 
+				visit '/users/1/posts/new' 
+				fill_in 'Title', with: 'Test Title'
+				fill_in 'Description', with: 'Test Description'
+				click_button 'Submit'
+				expect(page).to have_content 'Test Title'
+				expect(page).to have_content 'Test Description'
+			end 
 		end
 	end
 end	
